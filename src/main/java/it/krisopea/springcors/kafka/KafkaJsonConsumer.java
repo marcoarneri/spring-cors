53c4d6b72f6d8c8e4615cb9105b23b6ebf450602
arnerim <marco.arneri@tasgroup.eu>
2024-04-02 17:57:30 +0200

    aggiunti casi d'uso

--- src/main/java/it/krisopea/springcors/kafka/KafkaJsonConsumer.java
+++ src/main/java/it/krisopea/springcors/kafka/KafkaJsonConsumer.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import it.krisopea.springcors.service.dto.DemoRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,12 +21,22 @@ public class KafkaJsonConsumer {
         // Puoi qui fare qualsiasi elaborazione desideri con l'oggetto DemoRequestDto ricevuto
     }
 
-    @KafkaListener(topics = "kafkatest", groupId = "spring-cors")
-    public void listenWithHeaders(
-            @Payload String jsonMessage,
-            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) throws JsonProcessingException {
-        DemoRequestDto requestDto = objectMapper.readValue(jsonMessage, DemoRequestDto.class);
-        System.out.println(
-                "Received Message: " + requestDto + "from partition: " + partition);
-    }
+//    @KafkaListener(topics = "kafkatest", groupId = "spring-cors")
+//    public void listenWithHeaders(
+//            @Payload String jsonMessage,
+//            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) throws JsonProcessingException {
+//        DemoRequestDto requestDto = objectMapper.readValue(jsonMessage, DemoRequestDto.class);
+//        System.out.println(
+//                "Received Message: " + requestDto + "from partition: " + partition);
+//    }
+
+//    @KafkaListener(topicPartitions
+//            = @TopicPartition(topic = "kafkatest", partitions = { "0", "1" }))
+//    public void listenToPartition(
+//            @Payload String message,
+//            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+//        System.out.println(
+//                "Received Message: " + message
+//                        + "from partition: " + partition);
+//    }
 }
