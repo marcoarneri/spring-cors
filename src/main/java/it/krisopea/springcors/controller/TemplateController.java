04f6e7a14f090c9e44dcf001658aa7b86ff90828
GiampieroPorfiri <giampiero.porfiri@tasgroup.eu>
2024-04-04 18:02:56 +0200

    Fix

--- src/main/java/it/krisopea/springcors/controller/TemplateController.java
+++ src/main/java/it/krisopea/springcors/controller/TemplateController.java
@@ -6,13 +6,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-@Validated
 public class TemplateController {
   @GetMapping("/login")
   public String showLoginForm(ModelMap model) {
