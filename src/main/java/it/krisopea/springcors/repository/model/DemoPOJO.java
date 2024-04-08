991fd88594e3936b81203e669b61ec059a5f2b27
Grasso Giovanni <giovanni.grasso@tasgroup.eu>
2024-04-08 17:48:52 +0200

    wip

--- src/main/java/it/krisopea/springcors/repository/model/DemoPOJO.java
+++ src/main/java/it/krisopea/springcors/repository/model/DemoPOJO.java
@@ -3,7 +3,6 @@ package it.krisopea.springcors.repository.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
