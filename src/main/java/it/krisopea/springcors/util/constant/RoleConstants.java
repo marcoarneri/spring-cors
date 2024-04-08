c0169c75d600444055c1c4dac02ac361e38309af
GiampieroPorfiri <giampiero.porfiri@tasgroup.eu>
2024-04-08 17:32:28 +0200

    Removed annotations + added verified role

--- src/main/java/it/krisopea/springcors/util/constant/RoleConstants.java
+++ src/main/java/it/krisopea/springcors/util/constant/RoleConstants.java
@@ -2,5 +2,6 @@ package it.krisopea.springcors.util.constant;
 
 public interface RoleConstants {
   String ROLE_USER = "USER";
+  String ROLE_VERIFIED = "VERIFIED";
   String ROLE_ADMIN = "ADMIN";
 }
