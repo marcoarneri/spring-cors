14c74cfd060936084213ed9375a518c4cd4628c7
Andrea Rossi <andrea.rossi@tasgroup.eu>
2024-04-17 17:46:45 +0200

    Refactor

--- src/main/java/it/krisopea/springcors/config/security/WebSecurityConfig.java
+++ src/main/java/it/krisopea/springcors/config/security/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
@@ -76,6 +77,11 @@ public class WebSecurityConfig {
                     }));
 
     return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
