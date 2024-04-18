8df103c09d70fa3ff77422024636d2bffa479341
Andrea Rossi <andrea.rossi@tasgroup.eu>
2024-04-18 14:34:54 +0200

    Bug fixed

--- src/main/java/it/krisopea/springcors/repository/UserRepository.java
+++ src/main/java/it/krisopea/springcors/repository/UserRepository.java
@@ -14,15 +14,13 @@ public interface UserRepository
     extends JpaRepository<UserEntity, UUID>, JpaSpecificationExecutor<UserEntity> {
   Optional<UserEntity> findByUsername(String username);
 
-  @Query("SELECT u FROM UserEntity u JOIN u.roles r WHERE r.name = 'USER'")
+  @Query("SELECT u FROM UserEntity u JOIN u.roles r WHERE r.name NOT IN ('ADMIN', 'FOUNDER')")
   List<UserEntity> findAllUsers();
 
   @Query("SELECT u FROM UserEntity u JOIN u.roles r WHERE r.name = :name")
   List<UserEntity> findUsersByRoleName(String name);
 
-  @Query(
-      "SELECT u FROM UserEntity u WHERE NOT EXISTS (SELECT r FROM u.roles r WHERE r.name ="
-          + " 'FOUNDER')")
+  @Query("SELECT u FROM UserEntity u WHERE NOT EXISTS (SELECT r FROM u.roles r WHERE r.name = 'FOUNDER')")
   List<UserEntity> findAllNotFounder();
 
   Optional<UserEntity> findByEmail(String email);
