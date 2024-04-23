6bc92113f7646be81d569ea8fe412e8e21975ced
arnerim <marco.arneri@tasgroup.eu>
2024-04-23 09:18:30 +0200

    refactor angular controller

--- src/main/java/it/krisopea/springcors/controller/AngularController.java
+++ src/main/java/it/krisopea/springcors/controller/AngularController.java
@@ -8,6 +8,7 @@ import it.krisopea.springcors.service.mapper.MapperAngularClientDto;
 import it.krisopea.springcors.util.constant.PathConstants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,25 +26,28 @@ public class AngularController {
     private final MapperAngularClientDto mapperAngularClientDto;
 
     @GetMapping(PathConstants.GET_CLIENTS)
-    public List<AngularClientResponse> getClients() {
+    public ResponseEntity<List<AngularClientResponse>> getClients() {
         List<AngularClientResponseDto> usersDto = angularService.getClients();
         List<AngularClientResponse> users = mapperAngularClientDto.toClientResponse(usersDto);
-        return users;
+        return ResponseEntity.ok().body(users);
     }
 
     @PostMapping(PathConstants.ADD_CLIENT)
-    void addClient(@RequestBody AngularClientRequest request) {
+    public ResponseEntity<Void> addClient(@RequestBody AngularClientRequest request) {
         angularService.save(request);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping(PathConstants.UPDATE_CLIENT)
-    void updateClient(@RequestBody AngularClientRequest request){
+    public ResponseEntity<Void> updateClient(@RequestBody AngularClientRequest request){
         angularService.save(request);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/delete/{id}")
-    void deleteClient(@PathVariable("id") Long id) {
+    public ResponseEntity<Void> deleteClient(@PathVariable("id") Long id) {
         angularService.delete(id);
+        return ResponseEntity.ok().build();
     }
 
 }
